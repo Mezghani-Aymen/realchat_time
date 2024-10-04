5ea20b85e6dc281b9b144dbbf4fef6149c4bff5a
unknown <mezghanimohamedaymen@gmail.com>
2024-10-04 22:01:42 +0100

    [fix] add signup method service

--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,5 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user.schema';
+import { SignUpDto } from './dto/signup.dto';
+import * as bcrypt from 'bcrypt'
+
 @Injectable()
 export class AuthService {
-  constructor() { }
+  constructor(@InjectModel(User.name) private UserModle: Model<User>) { }
+
+  async signup(signUpData: SignUpDto) {
+    const { email, username, password } = signUpData
+    try {
+      const emailInUse = await this.UserModle.findOne({ email: email })
+      if (emailInUse) {
+        throw new BadRequestException("Email already in use");
+      }
+      const hashedPassword = await bcrypt.hash(password, 10)
+
+      await this.UserModle.create(
+        {
+          email, username, password: hashedPassword
+        })
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+
+  }
 }

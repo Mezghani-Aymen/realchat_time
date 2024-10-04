625a7edc98d70ed739dd1a5643657b96ec6a41ec
unknown <mezghanimohamedaymen@gmail.com>
2024-10-04 21:58:57 +0100

    [fix] add signup route

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -1,8 +1,13 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signup.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @Post('signup')
+  async signUp(@Body() signUpData: SignUpDto) {
+    return await this.authService.signup(signUpData);
+  }
 }

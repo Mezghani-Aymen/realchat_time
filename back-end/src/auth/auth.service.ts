17d6843d2d5f6577d9ac4f8171ce22b4423946dd
unknown <mezghanimohamedaymen@gmail.com>
2024-10-04 22:02:52 +0100

    [fix] add login method service

--- back-end/src/auth/auth.service.ts
+++ back-end/src/auth/auth.service.ts
@@ -1,13 +1,19 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from './schemas/user.schema';
 import { SignUpDto } from './dto/signup.dto';
 import * as bcrypt from 'bcrypt'
+import { JwtService } from '@nestjs/jwt';
+import { LogInDto } from './dto/login.dto';
+
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private UserModle: Model<User>) { }
+  constructor(
+    @InjectModel(User.name) private UserModle: Model<User>,
+    private jwtService: JwtService,
+  ) { }
 
   async signup(signUpData: SignUpDto) {
     const { email, username, password } = signUpData
@@ -22,6 +28,26 @@ export class AuthService {
         {
           email, username, password: hashedPassword
         })
+    } catch (error) {
+      throw new BadRequestException(error);
+    }
+
+  }
+
+  async login(logInData: LogInDto) {
+    const { email, password } = logInData
+    try {
+      const user = await this.UserModle.findOne({ email })
+      if (!user) {
+        throw new UnauthorizedException("Email invalide !");
+      }
+      const hashedPasswordCompare = await bcrypt.compare(password, user.password)
+
+      if (!hashedPasswordCompare) {
+        throw new UnauthorizedException("Password incorrect !");
+      }
+
+      return;
     } catch (error) {
       throw new BadRequestException(error);
     }

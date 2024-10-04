a7373cd4d9f25b8cf4d14a7a48766487858273c3
unknown <mezghanimohamedaymen@gmail.com>
2024-10-04 22:03:32 +0100

    [fix] add login route

--- back-end/src/auth/auth.controller.ts
+++ back-end/src/auth/auth.controller.ts
@@ -10,4 +10,9 @@ export class AuthController {
   async signUp(@Body() signUpData: SignUpDto) {
     return await this.authService.signup(signUpData);
   }
+
+  @Post('login')
+  async logIn(@Body() logInData: LogInDto) {
+    return await this.authService.login(logInData);
+  }
 }

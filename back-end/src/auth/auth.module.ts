90fbe0650d5234eae8bd3206ff0777e2d3f5ff5b
Mezghani-Aymen <mezghanimohamedaymen@gmail.com>
2024-10-05 16:44:08 +0100

    [fix] create refresh-token shcema

--- back-end/src/auth/auth.module.ts
+++ back-end/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { RefreshToken, RefreshTokenSchema } from './schemas/refresh-token.schema';
 
 @Module({
   imports: [MongooseModule.forFeature(
@@ -10,6 +11,10 @@ import { User, UserSchema } from './schemas/user.schema';
       {
         name: User.name,
         schema: UserSchema
+      },
+      {
+        name: RefreshToken.name,
+        schema: RefreshTokenSchema
       }
     ])
   ],
---
+++ back-end/src/auth/schemas/refresh-token.schema.ts
@@ -0,0 +1,16 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import mongoose, { Document } from "mongoose";
+
+@Schema({ versionKey: false, timestamps: true })
+export class RefreshToken extends Document {
+    @Prop({ required: true })
+    token: string
+
+    @Prop({ required: true, type: mongoose.Types.ObjectId })
+    userId: mongoose.Types.ObjectId
+
+    @Prop({ required: true })
+    expiryDate: Date
+}
+
+export const RefreshTokenSchema = SchemaFactory.createForClass(RefreshToken)
